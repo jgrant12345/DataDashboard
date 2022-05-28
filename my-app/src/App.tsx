d3fba3607643b02633396808bf83b69edb30fbbc
Justin Grant <justingrant99@me.com>
2022-05-27 19:21:42 -0700

    added google charts

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import { Chart } from "react-google-charts";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    yeah
+    <Chart
+  chartType="ScatterChart"
+  data={[["Age", "Weight"], [8, 12],[1, 4],[5, 7], [.2,.2]]}
+  width="100%"
+  height="400px"
+  legendToggle
+/>
     </div>
   );
 }
