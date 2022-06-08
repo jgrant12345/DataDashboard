40c644dafd1712c7a7162fd5989a3bc0d4f94039
Justin <justingrant99@me.com>
2022-06-07 19:33:56 -0700

    added svg

--- my-app/src/app/Components/Dashboard/Dashboard.tsx
+++ my-app/src/app/Components/Dashboard/Dashboard.tsx
@@ -1,13 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import "./DashBoard.scss"
 import { GraphContainer } from "../GraphContainer/GraphContainer";
 import Papa from "papaparse";
+import StatisticChart from "../StaticComponents/FinancialData.svg"
 export const Dashboard : React.FC<any> = () => {
+    const [chartData, changeChartData]: any = useState([]);
+  const [dataIsLoaded, changeDataIsLoaded] = useState(true);
+
+  useEffect(() => {
+    if(chartData.length > 0){
+      changeDataIsLoaded(false)
+    }
+  },[chartData]);
+  const changeFileFunction = (event: any) => {
+    const myPapaParse = Papa.parse(event.target.files[0], {
+      complete: (results) => changeChartData(results.data),
+  
+    });
+  };
+
+  if(dataIsLoaded){
+    return ( 
+    <div >
+        <div id="Container">
+    <div id="DownloadAndImageContainer">
+    <img  id="StatisticEmblem" src = {StatisticChart} />
+    </div>
+    <label id="inputLabel">
+ <input
+      type="file"
+      id="myFile"
+      name="filename"
+      onChange={changeFileFunction}
+    />
+    Upload CSV File 
+    </label>
+    </div>
+  
+</div>)
+   
+  }
+
+
     return (
-        <div>
-                  
+        <div>     
       <button>Upload CSV Here</button>
-            <GraphContainer ChartType= {"ScatterChart"} Height = {"98%"}
-            Width = {"98%"} data = {[[2,2], [3,4]]} 
+            <GraphContainer ChartType= {"ScatterChart"} Height = {"95%"}
+            Width = {"100%"} data = {[[2,2], [3,4]]} 
             />
             dashboard</div>
     )
--- my-app/src/app/Components/GraphContainer/GraphContainer.tsx
+++ my-app/src/app/Components/GraphContainer/GraphContainer.tsx
@@ -1,61 +1,70 @@
 import React, { useEffect, useState } from "react";
-import "./GraphContainer.scss"
+import "./GraphContainer.scss";
 import { Chart, GoogleChartWrapperChartType } from "react-google-charts";
 import Papa from "papaparse";
-
+import { resourceLimits } from "worker_threads";
 
 type Props = {
-    ChartType: GoogleChartWrapperChartType,
-    Width: string,
-    Height: string,
-    data: number[][]
-}
+  ChartType: GoogleChartWrapperChartType;
+  Width: string;
+  Height: string;
+  data: number[][];
+};
 
 export const data = [
-    ["Year", "Sales", "Expenses", "Profit"],
-    ["2014", 1000, 400, 200],
-    ["2015", 1170, 460, 250],
-    ["2016", 660, 1120, 300],
-    ["2017", 1030, 540, 350],
-  ];
-  
-  export const options = {
-    chart: {
-      title: "Company Performance",
-      subtitle: "Sales, Expenses, and Profit: 2014-2017",
-    },
-  };
-export const GraphContainer : React.FC<Props> = ({ChartType, Width, Height, data}) =>{
-  const [chartData, changeChartData] : any = useState([]);
-  const myFunction = (results: any) => {
-    console.log(results)
-        changeChartData(results);
-        console.log(chartData)
-  }
-  useEffect(() =>  {
-    console.log(chartData)
-  })
+  ["Year", "Sales", "Expenses", "Profit"],
+  ["2014", 1000, 400, 200],
+  ["2015", 1170, 460, 250],
+  ["2016", 660, 1120, 300],
+  ["2017", 1030, 540, 350],
+];
+
+export const options = {
+  chart: {
+    title: "Company Performance",
+    subtitle: "Sales, Expenses, and Profit: 2014-2017",
+  },
+};
+export const GraphContainer: React.FC<Props> = ({
+  ChartType,
+  Width,
+  Height,
+  data,
+}) => {
+  const [chartData, changeChartData]: any = useState([]);
+  const [dataIsLoaded, changeDataIsLoaded] = useState(true);
+
+  useEffect(() => {
+    if(chartData.length > 0){
+      changeDataIsLoaded(false)
+    }
+  });
   const changeFileFunction = (event: any) => {
+    const myPapaParse = Papa.parse(event.target.files[0], {
+      complete: (results) => changeChartData(results.data),
   
-    const myPapaParse = Papa.parse(event.target.files[0], {complete: results => changeChartData(results.data)
-  })}
+    });
+  };
 
+  return (
+    <div id="GraphContainer">
 
-
-    return (
-      
-        <div id="GraphContainer">
-          <form>
-  <input type="file" id="myFile" name="filename" onChange={changeFileFunction} />
-  <input type="submit" />
-    </form>
-        <Chart
-  chartType= {ChartType}
-  data={[["Age", "Weight"], data[0], data[1]]}
-  width= {Width}
-  height= {Height}
-  legendToggle
-/></div>
-    )
-}
-
+      <form>
+        <input
+          type="file"
+          id="myFile"
+          name="filename"
+          onChange={changeFileFunction}
+        />
+        <input type="submit" />
+      </form>
+      <Chart
+        chartType={ChartType}
+        data={[["Age", "Weight"], data[0], data[1]]}
+        width={Width}
+        height={Height}
+        legendToggle
+      />
+    </div>
+  );
+};
